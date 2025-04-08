aad2e32204ac1643029f54aa5e594bfd0abac1a4
Shreyansh Agrawal <shreyanshagrawal@Shreyanshs-MacBook-Pro.local>
2025-04-08 15:33:08 +0530

    pd fix

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -61,7 +61,7 @@ const Contact = () => {
   return (
     <div className="relative flex size-full min-h-screen flex-col bg-[#dad7cd] group/design-root overflow-hidden text-[#1d2923]" style={{ fontFamily: 'Inter, "Noto Sans", sans-serif' }}>
       <div className="layout-container flex h-full grow flex-col">
-        <div className="px-24 flex flex-1 justify-center py-24">
+        <div className="px-24 flex flex-1 justify-center py-12">
           <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
 
             <div className="flex flex-wrap justify-between gap-3 p-4">
